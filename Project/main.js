44fd254c7713934276cb0fcd4f1f6594c961526d
itzprem12 <itz.a.prem@gmail.com>
2024-07-08 06:35:51 +0530

    Add files via upload

---
+++ Project/main.js
@@ -0,0 +1,27 @@
+function redirect() {
+    window.location.href="index.html"
+}
+
+function redirect1() {
+    window.location.href="contact.html"
+}
+
+function redirect2() {
+    window.location.href="login.html"
+}
+
+function redirect3() {
+    window.location.href="signup.html"
+}
+
+function redirect4() {
+    window.location.href="register.html"
+}
+
+function redirect5() {
+    window.location.href="timer.html"
+}
+
+function redirect6() {
+    window.location.href="book.html"
+}
---
+++ Project/timer.js
@@ -0,0 +1,3 @@
+function redirect7() {
+    window.location.href="index.html"
+}
